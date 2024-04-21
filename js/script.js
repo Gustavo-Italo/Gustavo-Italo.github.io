d1fb9ece7a6408aa6a3bde4c16a2d9e8608059a3
Gustavo Italo <gussytalo@gmail.com>
2024-04-21 12:19:59 -0300

    alteracoes mobile

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+
+function clickMenu() {
+  
+  if (itens.style.display == 'block') {
+      itens.style.display = 'none';
+  } else {
+    itens.style.display = 'block';
+
+  }
+      
+}
+
+
+
+function blockchainMenu() {
+  
+  if (itens.blockchain.display == 'block') {
+      itens.blockchain.display = 'none';
+  } else {
+    itens.blockchain.display = 'block';
+
+  }
+      
+}
+
